fea99a75d4a9b31b55443ae66dd70c6714557989
Harry Northover <a-harry.northover@rvu.co.uk>
2025-05-04 19:00:13 +0100

    Settings Modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,16 @@ import { MD3DarkTheme, PaperProvider } from "react-native-paper";
 export default function RootLayout() {
   return (
     <PaperProvider theme={MD3DarkTheme}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            flexGrow: 1,
+            backgroundColor: MD3DarkTheme.colors.background,
+            paddingTop: 60,
+          },
+        }}
+      />
     </PaperProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { useGroupedRunData } from "@/hooks/useHealthData";
 import {
   HealthkitWriteAuthorization,
+  HKAuthorizationRequestStatus,
   HKQuantityTypeIdentifier,
   useHealthkitAuthorization,
 } from "@kingstinct/react-native-healthkit";
-import { Text, Button, Card, ProgressBar, useTheme } from "react-native-paper";
-import { ScrollView, View } from "react-native";
+import { Link } from "expo-router";
+import { ScrollView } from "react-native";
+import { Button, Card, Text, useTheme } from "react-native-paper";
 
 const saveableWorkoutStuff: readonly HealthkitWriteAuthorization[] = [
   "HKQuantityTypeIdentifierDistanceWalkingRunning",
@@ -34,50 +36,53 @@ export default function Index() {
       }}
     >
       <Text variant="headlineMedium" style={{ marginBottom: 16 }}>
-        Runs
+        Fastest Times
       </Text>
+
+      <Link href="/settings/modal">Open Settings</Link>
 
       {groupedRuns &&
         Object.entries(groupedRuns).map(([distance, runs]) => (
           <Card key={distance} style={{ marginBottom: 16 }}>
             <Card.Title
-              title={`${distance} Miles`}
-              subtitleVariant="headlineSmall"
+              title={`${distance} mi`}
+              titleVariant="headlineMedium"
+              titleStyle={{ fontWeight: "bold" }}
+              rightStyle={{ marginRight: 16 }}
+              right={({ size }) => (
+                <Text
+                  style={{
+                    fontSize: size * 0.8,
+                  }}
+                >
+                  {runs.fastestRun?.totalDistance?.quantity.toFixed(2)} mi in{" "}
+                  {Math.round(runs.fastestRun?.duration || 0) / 60} mins
+                </Text>
+              )}
             />
             <Card.Content>
-              <Text variant="bodyMedium" style={{ marginBottom: 8 }}>
-                Fastest Run is{" "}
-                {runs.fastestRun?.totalDistance?.quantity.toFixed(2)} mi in{" "}
-                {runs.fastestRun?.duration}s,{" "}
+              <Text variant="bodyMedium">
+                {runs.fastestRun?.startDate.toLocaleDateString()}{" "}
+                {runs.fastestRun?.startDate.toLocaleTimeString()}
               </Text>
-
-              {runs.runs.map((run, idx) => (
-                <View key={idx} style={{ marginBottom: 8 }}>
-                  <ProgressBar
-                    progress={
-                      run.duration / (runs.fastestRun?.duration || run.duration)
-                    }
-                    color={theme.colors.primary}
-                  />
-                  <Text variant="labelSmall">
-                    {Math.round(run.duration)}s –{" "}
-                    {run.totalDistance?.quantity.toFixed(2)} mi
-                  </Text>
-                </View>
-              ))}
+              <Text variant="bodyMedium" style={{ marginTop: 8 }}>
+                {runs.fastestRun?.totalEnergyBurned?.quantity} kcal
+              </Text>
             </Card.Content>
           </Card>
         ))}
 
-      <Button
-        mode="contained"
-        onPress={async () => {
-          const status = await requestAuthorization();
-          console.log("Authorization Status:", status);
-        }}
-      >
-        Request Authorization
-      </Button>
+      {authorizationStatus !== HKAuthorizationRequestStatus.unnecessary && (
+        <Button
+          mode="contained"
+          onPress={async () => {
+            const status = await requestAuthorization();
+            console.log("Authorization Status:", status);
+          }}
+        >
+          Request Authorization
+        </Button>
+      )}
     </ScrollView>
   );
 }
